aa16e97ee732af36700a5a6a3e866412853bb071
Hao Lee <haolee0702@engineering.ucla.edu>
2020-04-10 14:43:19 -0700

    before rewrite gradient eqns

--- main.m
+++ main.m
@@ -1,4 +1,148 @@
 % This script will generate a 2-d biped model's walking trajectories
 
 % the calculated torque profile will be used in openai gym box2d model
+clear;
+clc;
+syms q1 q2 q3 q4 q5 q6 q7
+syms q1_dot q2_dot q3_dot q4_dot q5_dot q6_dot q7_dot
+syms q1_dot2 q2_dot2 q3_dot2 q4_dot2 q5_dot2 q6_dot2 q7_dot2
 
+
+l_feet = 0.3048;
+l_calf = 0.45;
+l_thight =0.45;
+l_torso = 0.9;
+DH = [0, 0, 0, 0, 0, 0;...
+      0, l_feet, 0, 0, 0, 0;...
+      0, l_calf, 0, 0, 0, 0;...
+      0, l_thight, 0, 0, 0, 0;
+      0,l_thight, 0, 0, 0, 0;...
+      0,l_calf, 0, 0, 0, 0;...
+      0,l_feet, 0, 0, 0, 0];
+
+% DH = [0, 0, 0, 0, 0, 0;...
+%       0, l_feet, 0, 0, 0, 0;...
+%       0, l_calf, 0, 0, 0, 0;...
+%       0, l_thight, 0, 0, 0, 0;...
+%       pi, 0, 0 ,0,0,0;...
+%       0,l_thight, 0, 0, 0, 0];
+
+robot=DH2Robot(DH,1);
+
+% robot.plot([10,70,-15,35,180,15,-90,0]/180*pi);
+
+totM = 70; %kg
+m_feet = 0.0143*totM;
+m_calf = 0.0475*totM;
+m_thight = 0.105*totM;
+m_torso = totM - m_feet-m_calf-m_thight;
+
+
+robot.links(1).m = m_feet;
+robot.links(2).m = m_calf;
+robot.links(3).m = m_thight;
+% robot.links(4).m = m_torso;
+robot.links(4).m = m_thight;
+robot.links(5).m = m_calf;
+robot.links(6).m = m_feet;
+robot.links(7).m =0;
+
+
+robot.links(1).r = [l_feet/2,0,0]';
+robot.links(2).r = [l_calf/2,0,0]';
+robot.links(3).r = [l_thight/2,0,0]';
+% robot.links(4).r = [l_torso/2,0,0]';
+robot.links(4).r = [l_thight/2,0,0]';
+robot.links(5).r = [l_calf/2,0,0]';
+robot.links(6).r = [l_feet/2,0,0]';
+robot.links(7).r =[0,0,0]';
+
+I_feet = zeros(3);
+I_feet(3,3) =m_feet*l_feet^2/12;
+I_calf = zeros(3);
+I_calf(3,3) = m_calf*l_calf^2/12;
+I_thight = zeros(3);
+I_thight(3,3) = m_thight*l_thight^2/12;
+I_torso = zeros(3);
+I_torso(3,3) = m_torso*l_torso^2/12;
+robot.links(1).I = I_feet;
+robot.links(2).I = I_calf;
+robot.links(3).I = I_thight;
+% robot.links(4).I = I_torso;
+robot.links(4).I = I_thight;
+robot.links(5).I = I_calf;
+robot.links(6).I = I_feet;
+robot.links(7).I = zeros(3);
+
+% robot.links.r = [l_feet/2 0 0;...
+%           l_calf/2 0 0;...
+%           l_thight/2 0 0;...
+%           l_torso/2 0 0;...
+%           -l_thight/2 0 0;...
+%           -l_calf/2 0 0 ;...
+%           -l_feet/2 0 0 ].';
+
+
+robot.links(1).Jm=0;
+robot.links(2).Jm=0;
+robot.links(3).Jm=0;
+robot.links(4).Jm=0;
+robot.links(5).Jm=0;
+robot.links(6).Jm=0;
+robot.links(7).Jm=0;
+% robot.links(8).Jm=0;
+
+
+syms q1 q2 q3 q4 q5 q6 q7
+syms q1_dot q2_dot q3_dot q4_dot q5_dot q6_dot q7_dot
+syms q1_dot2 q2_dot2 q3_dot2 q4_dot2 q5_dot2 q6_dot2 q7_dot2
+mod.var = [q1 q2 q3 q4 q5 q6 q7];
+mod.var_dot = [q1_dot q2_dot q3_dot q4_dot q5_dot q6_dot q7_dot];
+mod.var_dot2 = [q1_dot2 q2_dot2 q3_dot2 q4_dot2 q5_dot2 q6_dot2 q7_dot2];
+syms fx fy fz
+mod.fext =[fx fy fz];
+
+syms g % it has to be a symbolic variable since we need to use it to solve G
+mod.g = g;
+mod.total_var = [q1 q2 q3 q4 q5 q6 q7 q1_dot q2_dot q3_dot q4_dot q5_dot q6_dot q7_dot q1_dot2 q2_dot2 q3_dot2 q4_dot2 q5_dot2 q6_dot2 q7_dot2 fx fy fz g];
+T0_1 = turnRTtoMatrix(robot.A([1],[q1,q2,q3,q4,q5,q6,q7,0]));
+T1_2 = turnRTtoMatrix(robot.A([2],[q1,q2,q3,q4,q5,q6,q7,0]));
+T2_3 = turnRTtoMatrix(robot.A([3],[q1,q2,q3,q4,q5,q6,q7,0]));
+T3_4 = turnRTtoMatrix(robot.A([4],[q1,q2,q3,q4,q5,q6,q7,0]));
+T4_5 = turnRTtoMatrix(robot.A([5],[q1,q2,q3,q4,q5,q6,q7,0]));
+T5_6 = turnRTtoMatrix(robot.A([6],[q1,q2,q3,q4,q5,q6,q7,0]));
+T6_7 = turnRTtoMatrix(robot.A([7],[q1,q2,q3,q4,q5,q6,q7,0]));
+T7_E = turnRTtoMatrix(robot.A([8],[q1,q2,q3,q4,q5,q6,q7,0]));
+
+p=[T0_1(1:3,4).';T1_2(1:3,4).';T2_3(1:3,4).';T3_4(1:3,4).';T4_5(1:3,4).';T5_6(1:3,4).';T6_7(1:3,4).';T7_E(1:3,4).'].';
+rMat = {T0_1(1:3,1:3),T1_2(1:3,1:3),T2_3(1:3,1:3),T3_4(1:3,1:3),T4_5(1:3,1:3),T5_6(1:3,1:3),T6_7(1:3,1:3),T7_E(1:3,1:3)};
+
+for i=1:length(mod.p)
+    p(i) = simplify(p(i));
+    rMat{i} = simplify(rMat{i});
+end
+mod.p = p;
+mod.rMat = rMat;
+
+
+
+
+I_feet = zeros(3);
+I_feet(3,3) =m_feet*l_feet^2/12;
+I_calf = zeros(3);
+I_calf(3,3) = m_calf*l_calf^2/12;
+I_thight = zeros(3);
+I_thight(3,3) = m_thight*l_thight^2/12;
+I_torso = zeros(3);
+I_torso(3,3) = m_torso*l_torso^2/12;
+mod.m = [m_feet,m_calf,m_thight,m_torso,m_thight,m_calf,m_feet];
+mod.Ic = {I_feet,I_calf,I_thight,I_torso,I_thight,I_calf,I_feet};
+
+
+      
+dynEqn = newtonEuler(mod);    
+% matlabFunction(dynEqn.tau,'File','Tau');
+matlabFunction(vpa(subs(dynEqn.M,g,9.81),5),'File','M_mat');
+matlabFunction(vpa(subs(dynEqn.G,g,9.81),5),'File','G_mat');
+matlabFunction(vpa(subs(dynEqn.F,g,9.81),5),'File','F_mat');
+% matlabFunction(dynEqn.V,'File','V_mat','Optimize',false);
--- newtonEuler.m
+++ newtonEuler.m
@@ -1,41 +1,77 @@
-function tou = newtonEuler(rMat,p,pc,m,Ic,var,var_dot,var_dot2,total_var)
-for i=1:length(total_var) 
-    syms(char(total_var(i))); 
+function dynEqn = newtonEuler(mod)
+%function tou = newtonEuler(rMat,p,pc,m,Ic,var,var_dot,var_dot2,total_var)
+for i=1:length(mod.total_var) 
+    syms(char(mod.total_var(i))); 
+    assume(mod.total_var(i),'real');
 end
-w=sym(zeros(3,length(var_dot)+1));
-w_dot=sym(zeros(3,length(var_dot)+1));
-w_dot2=sym(zeros(3,length(var_dot)+1));
-v_dot=sym(zeros(3,length(var_dot)+1));
-vc_dot=sym(zeros(3,length(var)));
-F=sym(zeros(3,length(var_dot)));
-f=sym(zeros(3,length(var_dot)+1));
-N=sym(zeros(3,length(var_dot)+1));
-n=sym(zeros(3,length(var_dot)+1));
+w=sym(zeros(3,length(mod.var_dot)+1));
+w_dot=sym(zeros(3,length(mod.var_dot)+1));
+w_dot2=sym(zeros(3,length(mod.var_dot)+1));
+v_dot=sym(zeros(3,length(mod.var_dot)+1));
+vc_dot=sym(zeros(3,length(mod.var)));
+F=sym(zeros(3,length(mod.var_dot)));
+f=sym(zeros(3,length(mod.var_dot)+1));
+N=sym(zeros(3,length(mod.var_dot)+1));
+n=sym(zeros(3,length(mod.var_dot)+1));
 %initialize v_dot(1) = [0,g,0]';
-v_dot(:,1)=vpa([-g 0 0].');
+g = mod.g; % I am just lazy here, did not want to change all the g terms 
+v_dot(:,1)=vpa([0 -g 0].');
 
 f(:,1)=[fx;fy;fz];
-for i=1:length(rMat)-1
-    rotM_temp=rMat{1,i};
-    w(:,i+1)=rotM_temp.'*w(:,i)+[0 0 var_dot(i)].';
-    w_dot(:,i+1)=rotM_temp.'*w_dot(:,i)+rotM_temp.'*cross(w(:,i),[0 0 var_dot(i)].')+[0 0 var_dot2(i)].';
-    v_dot(:,i+1)=rotM_temp.'*(cross(w_dot(:,i),p(:,i))+cross(w(:,i),cross(w(:,i),p(:,i)))+v_dot(:,i));
-    vc_dot(:,i+1)=cross(w_dot(:,i+1),pc(:,i))+cross(w(:,i+1),cross(w(:,i+1),pc(:,i)))+v_dot(:,i+1);
-    F(:,i+1)=m(i)*vc_dot(:,i+1);
-    N(:,i+1)=Ic{1,i}*w_dot(:,i+1)+cross(w(:,i+1),Ic{1,i}*w(:,i+1));
+for i=1:length(mod.rMat)-1
+    rotM_temp=mod.rMat{1,i};
+    w(:,i+1)=rotM_temp.'*w(:,i)+[0 0 mod.var_dot(i)].';
+    w_dot(:,i+1)=rotM_temp.'*w_dot(:,i)+rotM_temp.'*cross(w(:,i),[0 0 mod.var_dot(i)].')+[0 0 mod.var_dot2(i)].';
+    v_dot(:,i+1)=rotM_temp.'*(cross(w_dot(:,i),mod.p(:,i))+cross(w(:,i),cross(w(:,i),mod.p(:,i)))+v_dot(:,i));
+    vc_dot(:,i+1)=cross(w_dot(:,i+1),mod.pc(:,i))+cross(w(:,i+1),cross(w(:,i+1),mod.pc(:,i)))+v_dot(:,i+1);
+    F(:,i+1)=mod.m(i)*vc_dot(:,i+1);
+    N(:,i+1)=mod.Ic{1,i}*w_dot(:,i+1)+cross(w(:,i+1),mod.Ic{1,i}*w(:,i+1));
 end
 %now solve inward iteration
-rMat=fliplr(rMat);
+mod.rMat=fliplr(mod.rMat);
 N=fliplr(N);
 F=fliplr(F);
-pc=fliplr(pc);
-for i=1:length(rMat)-1
-    rotM_temp=rMat{1,i};
+mod.pc=fliplr(mod.pc);
+for i=1:length(mod.rMat)-1
+    rotM_temp=mod.rMat{1,i};
     f(:,i+1)=rotM_temp*f(:,i)+F(:,i);
-    n(:,i+1)=N(:,i)+rotM_temp*n(:,i)+cross(pc(:,i),F(:,i))+cross(p(:,i),rotM_temp*f(:,i));
+    n(:,i+1)=N(:,i)+rotM_temp*n(:,i)+cross(mod.pc(:,i),F(:,i))+cross(mod.p(:,i),rotM_temp*f(:,i));
 end
 n=fliplr(n);
-for i=1:length(rMat)-1
-    tou(1,i)=[0 0 1]*n(:,i);
+for i=1:length(mod.rMat)-1
+    tau(1,i)=[0 0 1]*n(:,i);
 end
-tou=vpa(tou,3);
+
+tau = vpa(tau,5);
+dynEqn.tau = tau;
+
+
+varLen = length(mod.var_dot);
+M = sym(zeros(varLen));
+G = sym(zeros(varLen,1));
+F = sym(zeros(varLen,3));
+
+for i=1:length(varLen)
+    for j=1:varLen
+        M(i,j) = simplify(diff(tau(1,i),mod.var_dot2(1,j)));
+    end
+    for j =1:length(mod.fext)
+        F(i,j) = simplify(diff(tau(1,i),mod.fext(j)));
+    end
+end
+G = simplify(diff(tau,g).');
+
+dynEqn.M=M;
+
+dynEqn.F = F;
+
+V = tau.'-dynEqn.M*mod.var_dot2.'-G*g-F*[fx;fy;fz];
+% for i =1:length(mod.var)
+%     V(i,1) = simplify(V(i,1));
+%     tau(1,i) = simplify(tau(1,i));
+%     
+% end
+dynEqn.V = V;
+dynEqn.G=G;
+% dynEqn.tau = tau;
+end
