c76e41518bc7e60c8bd3d53ede0f9ba33f043115
Hao Lee <haolee0702@engineering.ucla.edu>
2020-04-14 16:03:15 -0700

    generate sigma and sigma gradient

---
+++ five_links/act_fun_gen.m
@@ -0,0 +1,13 @@
+% create the sigma function that we use as activation function
+
+syms q1 q2 q3 q4 q5 th
+
+ypos = end_y_pos([q1,q2,q3,q4,q5]);
+
+sigma = 0.5*tanh(ypos-th);
+
+matlabFunction(sigma,'file','sigma_out');
+
+dsigma_dq = [diff(sigma,q1);diff(sigma,q2);diff(sigma,q3);diff(sigma,q4);diff(sigma,q5)];
+
+matlabFunction(dsigma_dq,'file','dsigma_dq');
